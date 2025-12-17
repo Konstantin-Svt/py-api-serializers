eeb66272533d75f1aec00937be71ec6397171538
Konstantin S. <kostaho16@gmail.com>
2025-12-17 20:12:00 +0200

    Solution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,23 @@
-# write urls here
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from cinema.views import (
+    CinemaHallViewSet,
+    ActorViewSet,
+    GenreViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+)
+
+router = DefaultRouter()
+router.register("cinema_halls", CinemaHallViewSet)
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("movies", MovieViewSet)
+router.register("movie_sessions", MovieSessionViewSet)
+
+app_name = "cinema"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,61 @@
-# write views here
+import typing
+
+from django.db.models import QuerySet
+from rest_framework import serializers
+from rest_framework.viewsets import ModelViewSet
+
+from cinema.models import Actor, Genre, CinemaHall, Movie, MovieSession
+from cinema.serializers import (
+    GenreSerializer,
+    CinemaHallSerializer,
+    MovieSerializer,
+    MovieListSerializer,
+    MovieCreateUpdateSerializer,
+    MovieSessionSerializer,
+    MovieSessionListSerializer,
+    MovieSessionCreateUpdateSerializer,
+    ActorFullNameSerializer,
+)
+
+
+class ActorViewSet(ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorFullNameSerializer
+
+
+class GenreViewSet(ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CinemaHallViewSet(ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieViewSet(ModelViewSet):
+    queryset = Movie.objects.prefetch_related("actors", "genres")
+
+    def get_serializer_class(self) -> typing.Type[serializers.Serializer]:
+        if self.action == "list":
+            return MovieListSerializer
+        elif self.action in ["create", "update", "partial_update"]:
+            return MovieCreateUpdateSerializer
+        return MovieSerializer
+
+
+class MovieSessionViewSet(ModelViewSet):
+    queryset = MovieSession.objects.all()
+
+    def get_queryset(self) -> QuerySet:
+        qs = self.queryset
+        if self.action == "list":
+            qs = qs.select_related("movie", "cinema_hall")
+        return qs
+
+    def get_serializer_class(self) -> typing.Type[serializers.Serializer]:
+        if self.action == "list":
+            return MovieSessionListSerializer
+        elif self.action in ["create", "update", "partial_update"]:
+            return MovieSessionCreateUpdateSerializer
+        return MovieSessionSerializer
